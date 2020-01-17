6760f01d544966084fe0480f2cabf3d4aa04b00c
Eraser <yrz12339@126.com>
2020-01-17 16:17:26 +0800

    LeetCode-7

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -5,18 +5,12 @@ class Solution {
 public:
     int reverse(int x) {
         long result = 0;
-        if(x == INT_MIN || x == 0) return 0;
-        while(x>0)
+        if(x == 0) return 0;
+        while(x!=0)
         {
-            result = result*10+x%10;
-            if(result>INT_MAX) return 0;
+            result = result*(10)+x%10;
+            if(result < INT_MIN || result > INT_MAX) return 0;
             x=x/10;
-        }
-        while(x<0)
-        {
-            result = result*(10)-(-x)%10;
-            if(result < INT_MIN) return 0;
-            x=-(-x)/10;
         }
         return result;
     }
