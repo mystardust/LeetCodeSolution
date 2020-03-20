276e779fe3318e8a41d89f8abf23b36aeb52771e
Eraser <yrz12339@126.com>
2020-03-20 15:35:31 +0800

    Recent Solutions

---
+++ CombinationSum2.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> combination;
+        sort(candidates.begin(), candidates.end());
+        traceback(result,candidates,combination,target,0);
+        return result;
+    }
+    void traceback(vector<vector<int>> &result, vector<int>& candidates, vector<int>& combination, int target, int start)
+    {
+        if(target == 0){result.push_back(combination); return;}
+        for(int i=start; i < candidates.size() && target >= candidates[i]; ++i)
+        {
+            if(i != start && candidates[i] == candidates[i-1]) continue;
+            combination.push_back(candidates[i]);
+            traceback(result, candidates, combination, target-candidates[i] , i);
+            combination.pop_back();
+        }
+    }
+};
---
+++ SodukuSolver.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+
+using namespace std;
+
+class Solution {
+
+    
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        
+    }
+};
